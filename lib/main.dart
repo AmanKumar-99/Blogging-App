125aad9aec165cb58f6bdc38ea5e75f26c87af61
aman <aman.kumar_cs18@gla.ac.in>
2020-10-13 19:54:17 +0530

    login page create

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'login/login.dart';
+
+void main() {
+  runApp(Login());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
